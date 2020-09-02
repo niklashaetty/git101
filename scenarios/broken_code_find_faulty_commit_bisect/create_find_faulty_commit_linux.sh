d819c56cc8b7e2cac3e2952c4e93760896cfb49b
Niklas Hätty <niklas.haetty@gmail.com>
2020-09-02 12:16:04 +0200

    Add linux version

---
+++ scenarios/broken_code_find_faulty_commit_bisect/create_find_faulty_commit_linux.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+SUCCESS="true"
+FAIL="false"
+WORD_LIST_FILE_PATH="/usr/share/dict/cracklib-small"
+
+mkdir scenario_find_faulty_commit
+chmod +x test.sh
+cp test.sh scenario_find_faulty_commit/
+cd scenario_find_faulty_commit
+git init
+touch test_results
+touch test_file_please_ignore
+
+echo $SUCCESS > test_results
+git add test_results
+git commit -m "INITIAL COMMIT WHERE TEST IS SUCCESSFUL"
+
+for i in {1..50}
+do
+  echo $SUCCESS > test_results
+  WORD=$(shuf -n1 $WORD_LIST_FILE_PATH)
+  WORD2=$(shuf -n1 $WORD_LIST_FILE_PATH)
+  echo $WORD > test_file_please_ignore
+  git add test_results
+  git add test_file_please_ignore
+  git commit -m "${WORD} ${WORD2}"
+done
+
+echo $FAIL > test_results
+WORD=$(shuf -n1 $WORD_LIST_FILE_PATH)
+WORD2=$(shuf -n1 $WORD_LIST_FILE_PATH)
+echo $WORD > test_file_please_ignore
+git add test_results
+git add test_file_please_ignore
+git commit -m "${WORD} ${WORD2}"
+
+for i in {1..25}
+do
+  echo $FAIL > test_results
+  WORD=$(shuf -n1 $WORD_LIST_FILE_PATH)
+  WORD2=$(shuf -n1 $WORD_LIST_FILE_PATH)
+  echo $WORD > test_file_please_ignore
+  git add test_results
+  git add test_file_please_ignore
+  git commit -m "${WORD} ${WORD2}"
+done
+
+
